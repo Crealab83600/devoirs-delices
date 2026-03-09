86c0e3fc1fc3ef9e7b8e7483fbc4024772fd2e0c
Crealab83600 <raphaelmaury83600@gmail.com>
2026-03-09 14:26:35 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+function reveal(){
 
+let elements=document.querySelectorAll(".reveal");
+
+elements.forEach(el=>{
+
+let windowHeight=window.innerHeight;
+let elementTop=el.getBoundingClientRect().top;
+
+if(elementTop < windowHeight - 150){
+el.classList.add("visible");
+}
+
+});
+
+}
+
+window.addEventListener("scroll",reveal);
